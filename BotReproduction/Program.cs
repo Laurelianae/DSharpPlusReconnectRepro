fc9d789282043a9b3ad4056a0dc58e60f1534723
Laurelianae <laurelianae@icloud.com>
2025-01-05 17:38:06 +0100

    Initial commit

---
+++ BotReproduction/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BotReproduction;
+using DSharpPlus;
+using DSharpPlus.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services
+    .AddDiscordClient(builder.Configuration.GetValue<string>("DISCORD_TOKEN", ""), DiscordIntents.AllUnprivileged)
+    .AddHostedService<BotHost>();
+
+var host = builder.Build();
+
+host.Run();
+
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# DSharpPlus Reconnect Reproduction
+
+## Configuration
+
+Token is set as DOTNET_DISCORD_TOKEN env variable.
+
